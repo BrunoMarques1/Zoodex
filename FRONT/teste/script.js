372385a62d269bfe496c859e77b61f5c4a578162
Bruno Marques <brunomarques.1@hotmail.com>
2024-09-24 15:14:20 -0300

    Update script.js

--- FRONT/teste/script.js
+++ FRONT/teste/script.js
@@ -5,7 +5,9 @@ const context = photo.getContext('2d');
 
 
 async function startCamera(cameraMode) {
-    const contraints = {video: {facinMode: cameraMode}}
+    const contraints = {video: {facinMode: {
+        exact: 'environment'
+      }}}
     const stream = await navigator.mediaDevices.getUserMedia({video:contraints})
     camera.srcObject = stream
 }
