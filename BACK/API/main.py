c197832af42ae6daaaab31a89b5ebeb1f3b89504
Bruno Marques <brunomarques.1@hotmail.com>
2024-09-30 18:22:19 -0300

    Update main.py

--- BACK/API/main.py
+++ BACK/API/main.py
@@ -19,7 +19,7 @@ lista = ["Lion"]
 
 @app.get("/")
 async def root():
-    return {"msg":"testegithubactions123"}
+    return {"msg":"testegithubactions123123"}
 
 @app.get("/animais")
 async def root():
