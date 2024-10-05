0bb59a952babec699cf8fa80d68d9e2e560eee5e
Bruno Marques <brunomarques.1@hotmail.com>
2024-10-04 22:17:44 -0300

    +

--- FRONT/teste/script.js
+++ FRONT/teste/script.js
@@ -1,3 +1,4 @@
+import {URL_API} from './config.js'
 document.addEventListener('DOMContentLoaded', function () {
     const camera = document.getElementById("camera")
     const photo = document.getElementById("photo")
@@ -9,8 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
     async function startCamera(facingMode) {
 
         if (stream) {
-            let tracks = stream.getTracks();
-            tracks.forEach(track => track.stop());  // Para todas as trilhas de vídeo
+            let tracks = stream.getTracks()
+            tracks.forEach(track => track.stop())  // Para todas as trilhas de vídeo
         }
         const constraints = {
             video: {
@@ -33,57 +34,69 @@ document.addEventListener('DOMContentLoaded', function () {
         camera.srcObject = stream
 
     }
+
+    function captureTeste(imagePath) {
+        const img = new Image()
+        img.src = imagePath
+        img.onload = function() {
+            context.clearRect(0, 0, photo.width, photo.height) 
+            context.drawImage(img, 0, 0, photo.width, photo.height)
+        }
+    }
     
     function captureFoto(){
         context.drawImage(camera, 0, 0, photo.width, photo.height)
     }
 
     async function uploadPhoto() {
-        const formData = new FormData();
+        const formData = new FormData()
         photo.toBlob(blob => {
-            formData.append('file', blob, 'photo.jpg');
+            formData.append('file', blob, 'photo.jpg')
 
-            fetch(process.env.urlAPI, {
+            //fetch("https://zoodex.site/detect/", {
+            fetch(URL_API, {
                 method: 'POST',
                 body: formData
             })
             .then(response => response.json())
             .then(data => {
-                resultDiv.innerHTML = '<h2>Detected Elemaents:</h2>';
+                resultDiv.innerHTML = '<h2>Detected Elemaents:</h2>'
                 //if (data.labels && data.labels.length > 0) {
                 //    data.labels.forEach(label => {
-                //        resultDiv.innerHTML += `<p>- ${label.name}: ${label.confidence}%</p>`;
-                //    });
+                //        resultDiv.innerHTML += `<p>- ${label.name}: ${label.confidence}%</p>`
+                //    })
 
-                resultDiv.innerHTML += `<p> Nome: ${data.nome}</p>`;
-                resultDiv.innerHTML += `<p> Descrição: ${data.descricao}</p>`;
+                resultDiv.innerHTML += `<p> Nome: ${data.nome}</p>`
+                resultDiv.innerHTML += `<p> Descrição: ${data.descricao}</p>`
                 console.log(data)
 //                    } else {
-//                        resultDiv.innerHTML += '<p>No elements detected in the image.</p>';
+//                        resultDiv.innerHTML += '<p>No elements detected in the image.</p>'
 //                    }
             })
             .catch(error => {
-                resultDiv.innerHTML = '<h2>Não foi possível detectar animais</h2>';
-                //console.error('Error uploading the photo:', error);
-            });
-        }, 'image/jpeg');
+                resultDiv.innerHTML = '<h2>Não foi possível detectar animais</h2>'
+                //console.error('Error uploading the photo:', error)
+            })
+        }, 'image/jpeg')
     }
 
+    
+
 //    async function getDevices() {
-//    const devices = await navigator.mediaDevices.enumerateDevices();
+//    const devices = await navigator.mediaDevices.enumerateDevices()
 //    
 //    // Limpa a lista de dispositivos antes de adicionar novos
-//    document.getElementById("devices").innerHTML = '';
+//    document.getElementById("devices").innerHTML = ''
 //
 //    for (const device of devices) {
 //        // Acessa o tipo de dispositivo e o nome
-//        document.getElementById("devices").innerHTML += `<p>${device.kind}: ${device.label || 'Sem nome'} | ${device.deviceId}</p>`;
+//        document.getElementById("devices").innerHTML += `<p>${device.kind}: ${device.label || 'Sem nome'} | ${device.deviceId}</p>`
 //    }
 //
-//    console.log(devices);
+//    console.log(devices)
 //    }
 
-    window.teste = teste
+    window.captureTeste = captureTeste
     window.startCamera = startCamera
     window.captureFoto = captureFoto
 //  window.getDevices = getDevices
