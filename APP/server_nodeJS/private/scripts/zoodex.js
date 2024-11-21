cee16bcc36d659ea7eecd2d6da85b9695515d00d
Bruno Marques <brunomarques.1@hotmail.com>
2024-11-21 01:20:38 -0300

    +

--- APP/server_nodeJS/private/scripts/zoodex.js
+++ APP/server_nodeJS/private/scripts/zoodex.js
@@ -1,4 +1,6 @@
 import { URL } from './url.js'
+const carrosel = document.getElementById('carrosel')
+
 function getInfo() {
     fetch(`${URL}/node_api/getZoodexInfo`, {
         method: 'GET',
@@ -6,17 +8,29 @@ function getInfo() {
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        data.analisados.forEach(element => {
-            document.getElementById('analisado').innerHTML += element.nome + '<br>'
+        data.analisados.forEach((element,index) => {
+            const isActive = index === 0 ? 'active' : ''; 
+            carrosel.innerHTML += `
+                <div class="carousel-item ${isActive}">
+                    <div class="profile">
+                        <div id="profile-pic-${element.id}" class="profile-pic"></div>
+                        <p class="nome-animal">${element.nome}</p>
+                        <p class="descricao">${element.descricao}</p>
+                    </div>
+                </div>`
+            document.getElementById(`profile-pic-${element.id}`).style = `background-image: url('${element.foto}');`
         });
-        data.naoAnalisados.forEach(element => {
-            document.getElementById('naoAnalisado').innerHTML += element.nome + '<br>'
-        });
+        //data.naoAnalisados.forEach(element => {
+        //    document.getElementById('naoAnalisado').innerHTML += element.nome + '<br>'
+        //});
     })
     .catch(error => console.error('Erro:', error))
 }
 
+function voltarIndex(){
+    window.location.href = ('/')
 
+}
 
 document.addEventListener('DOMContentLoaded', getInfo)
+document.getElementById('botaoVoltar').addEventListener('click', voltarIndex)
